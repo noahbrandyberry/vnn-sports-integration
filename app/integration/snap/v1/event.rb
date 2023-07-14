e0ff36d8a067d1ae4a0443543572db118cb55ed9
Noah Brandyberry <nbrandyberry@edgewebware.com>
2023-07-14 09:57:54 -0400

    chore: fix location

--- app/integration/snap/v1/event.rb
+++ app/integration/snap/v1/event.rb
@@ -14,7 +14,7 @@ module Snap
         home = place == 'H'
 
         location_search = home && school_location ? "#{location} #{school_location.city}" : location
-        location_results = location.present? ? Geocoder.search(location, locationbias: "point:#{school_location.latitude},#{school_location.longitude}") : []
+        location_results = location.present? ? Geocoder.search(location_search, locationbias: "point:#{school_location.latitude},#{school_location.longitude}") : []
         location_results = location_results.select {|result| school_location.distance_to(result.coordinates) < 75}
         location_result = location_results.sort_by {|result| school_location.distance_to(result.coordinates)}.first
 
