a09eda3c23f2680b48a5a0a36fc64dd691eadac4
Noah Brandyberry <nbrandyberry@edgewebware.com>
2023-10-07 20:52:28 -0400

    chore: fix final N+1 query

--- app/jobs/import_source_job.rb
+++ app/jobs/import_source_job.rb
@@ -4,7 +4,8 @@ class ImportSourceJob < ApplicationJob
   after_enqueue do |job|
     import_source = job.arguments.first
     import_source.update(last_import_time: nil)
-    import_source.events.destroy_all
+    TeamEvent.where(event_id: import_source.event_ids).delete_all
+    import_source.events.delete_all
   end
 
   def perform(import_source)
